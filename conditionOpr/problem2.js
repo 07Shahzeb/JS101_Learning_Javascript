60531bd3f63f3282042afc104800d9cb82de105d
07shahzeb <shahzebahmad202@gmail.com>
2023-02-06 13:13:10 +0530

    second commit

---
+++ conditionOpr/problem2.js
@@ -0,0 +1,9 @@
+//Problem 2: If a person is allowed to drive in India print "Apply for a license" or "NA".
+let age=17;
+let legalAge=18;
+if(age>=legalAge){
+  console.log("Apply for a license");
+}
+else{
+  console.log("NA");
+}
---
+++ conditionOpr/problem4.js
@@ -0,0 +1,23 @@
+// Problem 4: Given stored username and password and input username and password, Print if the user can login or not.
+
+let storedUserName="pw10_083";
+let storedPassword="*****";
+
+let username="pw10_083";
+let password="*****";
+
+if(storedUserName==username){
+  
+  if(storedPassword==password){
+    
+     console.log("login successful");
+  }  
+  else{
+  console.log("invalid password");
+  }
+  }
+  else{
+  console.log("invalid username");
+  }
+
+
